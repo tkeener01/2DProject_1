4b125c7fc2fea89b27771b0ab87c625fb44b7cb5
Jules-1948 <schamingjc20@gcc.edu>
2023-09-30 22:01:49 -0400

    Exciting Updates!!
    Added .png's, updated the maze layout, fixed the physics for the roomba, and made it successfully disappear behind the goal (changed the sprite renderer sorting layer)

--- 2DProject_1_Unity/Assets/Scripts/PlayerScript.cs
+++ 2DProject_1_Unity/Assets/Scripts/PlayerScript.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.Experimental.GraphView;
@@ -5,49 +6,100 @@ using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
 [RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(SortingLayer))]
+[RequireComponent(typeof(Renderer))]
 public class PlayerScript : MonoBehaviour
 {
 
-    public float speed;
+    public float _speed;
     Rigidbody2D _rbody;
-    SpriteRenderer _srenderer;
-    Vector2 direction = new Vector2(0,0);
+    SpriteRenderer _spriteRenderer;
+    Renderer _renderer;
+
+    bool _playing = true;
+    bool _moving = false;
     // Start is called before the first frame update
     void Start()
     {
         _rbody = GetComponent<Rigidbody2D>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-   
-        //base direction vector
-        float xDir = Input.GetAxis("Horizontal");
-        float yDir = 0;
-        if (xDir == 0)
-        {
-            yDir = Input.GetAxis("Vertical");
+        if (!_playing) {
+            endGame();
+            return;
         }
-        Vector2 direction = (new Vector2(xDir, yDir)).normalized;
 
+        print("currentMangnitude: " + _rbody.velocity.magnitude);
+        if(_rbody.velocity.magnitude <= 0 )
+        {
+            _moving = false;
+            _rbody.constraints = RigidbodyConstraints2D.FreezeRotation;
+        }
 
-        //set direction based on direction
-        _rbody.velocity = direction * speed;
-
-
-        //continue sliding if not stopped by wall
-        Vector2 current = _rbody.velocity;
-        direction = current.normalized;
-        Vector2 newVelocity = direction * speed;
-        _rbody.velocity = newVelocity;
-        
+        float force = _speed * Time.deltaTime;
+        if (!_moving)
+        {
+            // get player input
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                _rbody.velocity = Vector2.up * force;
+                _rbody.rotation = 0; 
+                _moving = true;
+            }
+            else if (Input.GetKey(KeyCode.DownArrow))
+            {
+                _rbody.velocity = Vector2.down * force;
+                _rbody.rotation = 180;
+                _moving = true;
+            }
+            else if (Input.GetKey(KeyCode.RightArrow))
+            {
+                _rbody.velocity = Vector2.right * force;
+                _rbody.rotation = 270;
+                _moving = true;
+            }
+            else if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                _rbody.velocity = Vector2.left * force;
+                _rbody.rotation = 90;
+                _moving = true;
+            }
+        }else
+        {
+            //player keeps moving in chosen direction
+            Vector2 current = _rbody.velocity.normalized;
+            float x = Math.Abs(current.x);
+            float y = Math.Abs(current.y);
+            if (x >= y) //restricts movement in any other direction
+            {
+                _rbody.constraints = RigidbodyConstraints2D.FreezePositionY | RigidbodyConstraints2D.FreezeRotation;
+            }
+            else {
+                _rbody.constraints = RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezeRotation;
+            }
+            Vector2 newVelocity = current * force;
+            _rbody.velocity = newVelocity;
+        }
 
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        _srenderer.color = Color.blue;
+        if (collision.gameObject.tag.ToString() == "Goal")
+        {
+            _playing = false;
+        }
+    }
+
+    private void endGame()
+    {
+        _rbody.GetComponent<Renderer>().sortingLayerID = SortingLayer.NameToID("Default");
+        //display score and time
     }
 
 
