3cc3101d7bae07bae06b94f0a6cd69be810401af
tkeener01 <keenervl20@gcc.edu>
2023-10-01 22:18:35 -0400

    Coins disappear after 10 seconds

--- 2DProject_1_Unity/Assets/Scripts/MSManagerScript.cs
+++ 2DProject_1_Unity/Assets/Scripts/MSManagerScript.cs
@@ -10,6 +10,8 @@ public class MSManagerScript : MonoBehaviour
     public Text _movesText;
     public int moves;
 
+    private float _startTime;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,13 +21,26 @@ public class MSManagerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if((int)(Time.time - _startTime) > 10)
+        {
+            GameObject[] gos = GameObject.FindGameObjectsWithTag("Ring");
+            foreach (GameObject go in gos)
+                Destroy(go);
+        }
         _movesText.text = "Moves: " + moves.ToString();
     }
 
     public void HitCoin()
     {
+        _startTime = Time.time;
         Instantiate(_prefabRing, new Vector3(10,5,0), Quaternion.identity);
         Instantiate(_prefabRing, new Vector3(1.5f, 5, 0), Quaternion.identity);
         Instantiate(_prefabRing, new Vector3(1.5f, 2.5f, 0), Quaternion.identity);
+        Instantiate(_prefabRing, new Vector3(7.5f, 2.5f, 0), Quaternion.identity);
+        Instantiate(_prefabRing, new Vector3(8.5f, .25f, 0), Quaternion.identity);
+        Instantiate(_prefabRing, new Vector3(13.5f, .25f, 0), Quaternion.identity);
+        Instantiate(_prefabRing, new Vector3(1, -4.5f, 0), Quaternion.identity);
+        Instantiate(_prefabRing, new Vector3(-1.5f, 2.5f, 0), Quaternion.identity);
+        Instantiate(_prefabRing, new Vector3(-9.5f, 4.3f, 0), Quaternion.identity);
     }
 }
